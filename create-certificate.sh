3fc55920509eff279185209c68129945a52869d4
Andy Schott <andy@andyschott.com>
2025-09-14 23:13:38 +0000

    deploy biblereadings

--- create-certificate.sh
+++ create-certificate.sh
@@ -3,16 +3,8 @@
 rsa_key_size=4096
 data_path="./certbot"
 email="andy@andyschott.com"
-declare -a domains=("primarycolorsman.com")
+declare -a domains=("biblereadings.primarycolorsman.com")
 dry_run="0"
-
-if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
-    echo "### Downloading recommended TLS parameters ..."
-    mkdir -p "$data_path/conf"
-    curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf >"$data_path/conf/options-ssl-nginx.conf"
-    curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem >"$data_path/conf/ssl-dhparams.pem"
-    echo
-fi
 
 domain_args=""
 for domain in "${domains[@]}"; do
